107c13781627845d2a28daebef71a16222da964f
ahnzhx <modunaeggu@naver.com>
2019-03-24 15:35:14 +0900

    setting

--- src/main/java/com/example/sonniespringdev/SonnieSpringDevApplication.java
+++ src/main/java/com/example/sonniespringdev/SonnieSpringDevApplication.java
@@ -1,16 +1,13 @@
 package com.example.sonniespringdev;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-@PropertySource("classpath:/application.properties")
 public class SonnieSpringDevApplication {
 
 	@GetMapping("/hello")
@@ -20,7 +17,7 @@ public class SonnieSpringDevApplication {
 
 	public static void main(String[] args) {
 		//웹 어플리케이션 띄우지 않기
-//		SpringApplication app = new SpringApplication(SonnieSpringDevApplication.class);
+//		SpringApplication app  = new SpringApplication(SonnieSpringDevApplication.class);
 //		app.setWebApplicationType(WebApplicationType.NONE);
 //		app.run(args);
 
