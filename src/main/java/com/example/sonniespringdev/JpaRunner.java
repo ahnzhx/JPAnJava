c779fbe3eb1ede18cdc7b5d16d09e017bb4a56bc
ahnzhx <modunaeggu@naver.com>
2019-04-27 22:11:16 +0900

    jpa:

--- src/main/java/com/example/sonniespringdev/JpaRunner.java
+++ src/main/java/com/example/sonniespringdev/JpaRunner.java
@@ -1,34 +1,21 @@
 package com.example.sonniespringdev;
 
-import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 @Component
 @Transactional
 public class JpaRunner implements ApplicationRunner {
 
-    @PersistenceContext
-    EntityManager entityManager;
+    @Autowired
+    PostRepository postRepository;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Post post = new Post();
-        post.setTitle("spring data jpa 언제시작하나요");
-
-        Comment comment = new Comment();
-        comment.setComment("we'll start very soon");
-        post.addComment(comment);
-
-        Comment comment1 = new Comment();
-        comment1.setComment("you good at hiberante");
-        post.addComment(comment1);
-
-        Session session = entityManager.unwrap(Session.class);
-        session.save(post);
+        postRepository.findAll().forEach(System.out::println);
     }
 }
